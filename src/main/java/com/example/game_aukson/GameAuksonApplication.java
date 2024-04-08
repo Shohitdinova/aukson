d67c41f24268b591f4a7ecdc6cba028173926548
Moxina <mohina@gmail.com>
2024-04-08 16:49:05 +0500

    game aukson

---
+++ src/main/java/com/example/game_aukson/GameAuksonApplication.java
@@ -0,0 +1,13 @@
+package com.example.game_aukson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameAuksonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameAuksonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/game_aukson/GameAuksonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.game_aukson;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameAuksonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
