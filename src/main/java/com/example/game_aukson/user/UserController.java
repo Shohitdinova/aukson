683eb657f13a12d2ccb1ec789689c2d0bec32f97
Moxina <mohina@gmail.com>
2024-04-09 16:06:15 +0500

    game aukson html

---
+++ src/main/java/com/example/game_aukson/user/UserController.java
@@ -0,0 +1,37 @@
+package com.example.game_aukson.user;
+
+import com.example.game_aukson.user.entity.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+
+@RequestMapping("/users")
+@RequiredArgsConstructor
+public class UserController {
+    private final UserService userService;
+
+    @PostMapping("/Create a new user")
+    public User createUser(@RequestBody User user) {
+        return userService.createUser(user);
+
+    }
+
+    @GetMapping("/Get all users")
+    public List<User> getAllUsers() {
+        return userService.getAllUsers();
+    }
+
+    @PutMapping("/{id}")
+    public User updateUser(@PathVariable Long id, @RequestBody User newUser) {
+        return userService.updateUser(id, newUser);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteUser(@PathVariable Long id) {
+        userService.deleteUser(id);
+    }
+}
---
+++ src/main/java/com/example/game_aukson/user/UserService.java
@@ -0,0 +1,33 @@
+package com.example.game_aukson.user;
+import com.example.game_aukson.user.entity.User;
+
+import java.util.List;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+    private final UserRepository userRepository;
+
+    public User createUser(User user) {
+        return userRepository.save(user);
+    }
+
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    public User updateUser(Long id, User newUser) {
+        newUser.setId(id);
+        return userRepository.save(newUser);
+    }
+
+    public void deleteUser(Long id) {
+        userRepository.deleteById(id);
+    }
+
+
+}
